4685d6fd3b0df06c23efd102135db92222762c1e
JKS-dev <dms2535saini@gmail.com>
2024-03-09 16:10:07 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,8 @@ export default async function RootLayout({
     <html lang="en" >
       <body>
       <SessionProvider>
+      <SpeedInsights/>
+      <Analytics/>
         <NavBar />
         {children}
       </SessionProvider>
