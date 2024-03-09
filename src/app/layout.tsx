346a90c53cc71fa441b37d7574811a619f282196
JKS-dev <dms2535saini@gmail.com>
2024-03-09 15:52:08 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import { NavBar } from '@/components/NavBar/NavBar';
 import SessionProvider from './SessionProvider';
 import "@/styles/globals.css"
-
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 export default async function RootLayout({
   children,
 }: {
